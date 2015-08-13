6f301ae1032d14a30faa371b9f8760e0f9d96108
3oc <TAKAHIDE@H.local>
2015-08-13 01:00:56 -0500

    something

---
+++ public/script/lp.js
@@ -0,0 +1,51 @@
+$(function () {
+    $("#menu_01").click(function () {
+        if ($(".about").hasClass("open")) {
+            $(".about").removeClass("open");
+            $(".about").slideToggle();
+            $(".bg").animate({
+                "height": "1000px"
+            }, 1000);
+            $("#menu_01").attr("src", "/assets/about_btn.png");
+	    $("section.top .menu_btn img").css({width: "49.5%", "max-width": "300px"});
+	    
+        } else {
+            $(".about").slideToggle();
+            $(".bg").animate({
+                "height": "2000px"
+            }, 1000);
+            $(".about").addClass("open");
+	    $("#menu_01").attr("src", "/assets/howto_title.png");
+            $("section.top .menu_btn img").css({width: "70%", "max-width": "500px"});
+        }
+    });
+});
+$(function () {
+    $("#menu_02").click(function () {
+        if ($(".usable").hasClass("open")) {
+            $(".usable").removeClass("open");
+            $(".usable").slideToggle();
+            if ((".about").hasClass("open")) {
+                $(".bg").animate({
+                    "height": "2000px"
+                }, 1000);
+            } else {
+                $(".bg").animate({
+                    "height": "1000px"
+                }, 1000);
+            }
+        } else {
+            $(".usable").slideToggle();
+            $(".usable").addClass("open");
+            if ((".about").hasClass("open")) {
+                $(".bg").animate({
+                    "height": "3000px"
+                }, 1000);
+            } else {
+                $(".bg").animate({
+                    "height": "1500px"
+                }, 1000);
+            }
+        }
+    });
+});
